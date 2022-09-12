5db985d78a5b6da00698c77277611f238f9e28b5
Anna Li <halloannali@gmail.com>
2022-09-12 19:47:13 +0200

    cleaned up repo

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
-import logo from "./logo.svg";
-
+import { Heading, Text, Wrap, Card } from "@vygruppen/spor-react";
 import "./App.css";
+import { color } from "@chakra-ui/react";
 
 function App() {
   const [cats, setCats] = useState([]);
+  const cardColors = [
+    "white",
+    "grey",
+    "blue",
+    "green",
+    "teal",
+    "yellow",
+    "orange",
+  ];
+  function get_random(list) {
+    return list[Math.floor(Math.random() * list.length)];
+  }
+
+  console.log(get_random(cardColors));
 
   useEffect(() => {
     const fetchData = async () => {
@@ -23,40 +37,44 @@ function App() {
   }, [cats]);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>I am using CRA</p>
-        <p>
-          Lorem ipsum dolor sit amet, at quo meis nonumy, mel no causae
-          salutandi iudicabit, et iuvaret appellantur cum. Vis aperiri principes
-          ei, id qui doctus instructior signiferumque. Ut alia vidit quo, no pri
-          sint possim vocent. Ad vim mollis facilisi dissentias, ne vis saperet
-          adipiscing. Ex nisl populo saperet mea. Hinc labore sanctus pri no,
-          pro in intellegebat conclusionemque. Veniam nusquam eam no, sit
-          minimum fabellas reformidans cu, ei tation tempor nonumes mea. Pro an
-          ipsum tation.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <span>{`Dagens dato: ${moment()}`}</span>
-        {cats.map((cat) => (
-          <figure key={cat.id}>
-            <img
-              id={cat.id}
-              src={cat.url}
-              alt={cat.alt}
-              width="200px"
-              height="200px"
-            />
-          </figure>
-        ))}
+    <div className="app">
+      <header className="app-header">
+        <Heading textStyle="2xl">Velkommen til SSR workshop</Heading>
+        <Text textStyle="md" pt={6} pb={8}>
+          Denne appen er laget med <b>Create React App</b>. Oppgaven din er å
+          migrere den over til <b>Next.js</b>. Oppskrift for hvordan det gjøres
+          finner du{" "}
+          <a href="https://nextjs.org/docs/migrating/from-create-react-app">
+            her
+          </a>
+          .
+        </Text>
       </header>
+      <main className="app-main">
+        <Wrap gap={3} justify="center">
+          {cats.map((cat) => (
+            <Card
+              key={cat.id}
+              colorScheme={get_random(cardColors)}
+              p={4}
+              size="lg"
+              width="fit-content"
+            >
+              <figure key={cat.id}>
+                <img
+                  id={cat.id}
+                  src={cat.url}
+                  alt={cat.alt}
+                  width="400px"
+                  height="400px"
+                />
+                <figcaption>hello</figcaption>
+              </figure>
+              <span>{`Dagens dato: ${moment().format("LL")}`}</span>
+            </Card>
+          ))}
+        </Wrap>
+      </main>
     </div>
   );
 }
--- cra/src/index.js
+++ cra/src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { SporProvider, Language } from "@vygruppen/spor-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <SporProvider language={Language.English}>
+      <App />
+    </SporProvider>
   </React.StrictMode>
 );
 
