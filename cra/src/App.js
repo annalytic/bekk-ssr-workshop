f3075d01fa3415862834b91285e6e43f94cc44b3
unknown <kristine.skjellestad@bekk.no>
2022-09-13 17:14:24 +0200

    skiftet til rick and morty api

--- cra/src/App.js
+++ cra/src/App.js
@@ -11,7 +11,7 @@ const AppContainer = styled.div`
 `;
 
 function App() {
-  const [cats, setCats] = useState([]);
+  const [characters, setCharacters] = useState([]);
   const cardColors = [
     "white",
     "grey",
@@ -29,37 +29,39 @@ function App() {
   useEffect(() => {
     const fetchData = async () => {
       const resp = await fetch(
-        "https://api.thecatapi.com/v1/images/search?limit=10"
+        "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10"
       );
-      const cats = await resp.json();
-      setCats(cats);
+      const characters = await resp.json();
+      setCharacters(characters);
     };
     fetchData();
   }, []);
+
+  console.log("characters", characters);
 
   return (
     <AppContainer>
       <Header />
       <main className="app-main">
         <Wrap gap={3} justify="center">
-          {cats.map((cat) => (
+          {characters.map((character) => (
             /* Dette må burde flyttes inn i en Card komponent */
             <Card
-              key={cat.id}
+              key={character.id}
               colorScheme={getRandom(cardColors)}
               p={4}
               size="lg"
               width="fit-content"
             >
-              <figure key={cat.id}>
+              <figure key={character.id}>
                 <img
-                  id={cat.id}
-                  src={cat.url}
-                  alt={cat.alt}
+                  id={character.id}
+                  src={character.image}
+                  alt={character.name}
                   width="400px"
                   height="400px"
                 />
-                <figcaption>hello</figcaption>
+                <figcaption>{character.name}</figcaption>
               </figure>
               <span>{`Dagens dato: ${moment().format("LL")}`}</span>
             </Card>
